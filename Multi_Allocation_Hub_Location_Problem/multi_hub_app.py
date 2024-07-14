7b274113adc30cca12090ff7041d4eb45bf4c844
Lazuardis <110012138+Lazuardis@users.noreply.github.com>
2024-07-14 11:02:27 +0700

    push

--- Multi_Allocation_Hub_Location_Problem/multi_hub_app.py
+++ Multi_Allocation_Hub_Location_Problem/multi_hub_app.py
@@ -94,7 +94,7 @@ if st.button('Calculate'):
 
     # Solve the model
     # opt = pyo.SolverFactory('cbc', executable='Multi_Allocation_Hub_Location_Problem\\bin\\cbc.exe')  # Change solver if needed
-    opt = pyo.SolverFactory('cbc', executable='bin\\cbc.exe')  # Change solver if needed
+    opt = pyo.SolverFactory('cbc', executable='Multi_Allocation_Hub_Location_Problem/bin/cbc.exe')  # Change solver if needed
     results = opt.solve(model)
 
     # Extract the objective function value
